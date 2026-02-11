108862a131b9ed1efc07e44b8bee0222cfe427ef
Anubhav575 <anubhavsinghcontact@gmail.com>
2026-02-11 11:14:19 +0530

    Add files via upload

---
+++ anubhav.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    if (a == b)
+    { 
+        // what is the error
+        printf("no are equal");
+    }
+    else {
+        printf("no are not equal");
+    }
+
+    return 0;
+}
