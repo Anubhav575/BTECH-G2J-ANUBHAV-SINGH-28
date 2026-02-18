c7bf76edd8fe55d755c588484c8d3899ebd9ce8e
Anubhav575 <anubhavsinghcontact@gmail.com>
2026-02-18 12:03:52 +0530

    Add files via upload

---
+++ vowels.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char ch ;
+    printf("enter the value");
+    scanf("%c",&ch);
+    if( ch=='a||e||i||o||u')
+    {
+    printf("the value is true");}
+    else
+    {
+    printf("the value is constonants");
+    }
+return 0;}
+
